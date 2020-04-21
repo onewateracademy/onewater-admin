4012cfcedc50bf0a3c432f27d26b7bb81d900f77
mohitpatell <mohit@wednesday.is>
2020-04-22 00:58:11 +0530

    prime video done

--- src/app/video/landing-video/landing-video.component.ts
+++ src/app/video/landing-video/landing-video.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../service/common.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-video',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-video.component.scss']
 })
 export class LandingVideoComponent implements OnInit {
-
-  constructor() { }
+  video;
+  form: FormGroup;
+  constructor(public common: CommonService) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      link: new FormControl(null,{validators:[Validators.required]})
+    })
+    this.common.getPrimeVideo()
+    .subscribe(result=> {
+      console.log(result)
+      this.video=result.result
+    })
+  }
+
+  submit() {
+    if(this.form.invalid) {
+      return alert("Enter Link");
+    }
+    this.common.updatePrimeVideo(this.form.value.link)
+    .subscribe(result=> {
+      console.log(result);
+      alert("Video Updated")
+    })
   }
 
 }
